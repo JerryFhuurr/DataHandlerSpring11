74250201b793e5c76e2c484a481c0a2a8ca03f1b
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2022-10-02 00:28:41 +0800

    Add type mapper

---
+++ src/main/java/com/spring/handler/datahandlerspring11/sqlmapper/TypeMapper.java
@@ -0,0 +1,23 @@
+package com.spring.handler.datahandlerspring11.sqlmapper;
+
+import com.spring.handler.datahandlerspring11.model.Type;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface TypeMapper {
+    void addSingleType(Type type);
+
+    void addMoreType(List<Type> types);
+
+    void removeType(int id);
+
+    void removeTypes(List<Integer> ids);
+
+    void updateType(Type type);
+
+    Type getSingleType(Type type);
+
+    List<Type> getAllTypes();
+}
