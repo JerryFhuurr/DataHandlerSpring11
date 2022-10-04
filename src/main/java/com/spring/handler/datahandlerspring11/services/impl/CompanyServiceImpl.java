91e68eb53759ddf9b4f051ecb2335d95335c6610
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2022-10-04 15:02:52 +0800

    Update controllers

--- src/main/java/com/spring/handler/datahandlerspring11/services/impl/CompanyServiceImpl.java
+++ src/main/java/com/spring/handler/datahandlerspring11/services/impl/CompanyServiceImpl.java
@@ -1,6 +1,7 @@
 package com.spring.handler.datahandlerspring11.services.impl;
 
 import com.spring.handler.datahandlerspring11.model.Company;
+import com.spring.handler.datahandlerspring11.model.Type;
 import com.spring.handler.datahandlerspring11.services.CompanyService;
 import com.spring.handler.datahandlerspring11.sqlmapper.BangumiCompanyMapper;
 import org.apache.ibatis.annotations.Param;
@@ -16,14 +17,46 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public String addCompany(Company company) {
-        mapper.addCompany(company);
-        return "Company id : " + company.getCompanyId() + " added";
+        Company companyGet = mapper.getCompanyById(company.getCompanyId());
+        if (companyGet != company) {
+            mapper.addCompany(company);
+            return "Company id : " + company.getCompanyId() + " added";
+        } else {
+            return "ERROR: The type is already existed.";
+        }
     }
 
     @Override
     public String addMoreCompanies(List<Company> companies) {
-        mapper.addMoreCompanies(companies);
-        return companies.size() + " companies added";
+        String companyVerify = verifyAddMore(companies);
+        if (companyVerify.equals("null")) {
+            mapper.addMoreCompanies(companies);
+            return companies.size() + " companies added";
+        } else {
+            return companyVerify;
+        }
+    }
+
+    private String verifyAddMore(List<Company> companies) {
+        List<Company> companyList = mapper.getAllCompany();
+        String errorFront = "ERROR: The type id ";
+        int errorCount = 0;
+        for (var company :
+                companies) {
+            for (var companyGet :
+                    companyList) {
+                if (company == companyGet) {
+                    errorFront += company.getCompanyId();
+                    errorCount++;
+                }
+            }
+        }
+        if (errorCount == 0) {
+            errorFront = "null";
+        } else {
+            errorFront += "(is) are duplicated";
+        }
+        return errorFront;
     }
 
     @Override
